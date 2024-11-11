d5d5392f76fb91b6a32da3bb20381087baa35b43
Guillermo Rodríguez Rodríguez <grodriguez@novationits.com>
2024-11-11 11:36:12 +0100

    Añadida pausa para salir del programa

--- proyecto/main.c
+++ proyecto/main.c
@@ -4,11 +4,16 @@
 
 */
 
+#include <conio.h>
 #include <stdio.h>
 
 int main()
 {
     printf( "Hola mundo." );
+
+    printf( "\n\n   Pulse una tecla para salir..." );
+
+    getch(); /* Pausa */
 
     return 0;
 }
